57b55cc8bfdc323a5042d3ddcafb7624b24b555b
Krinza Momin <44579545+kaymomin@users.noreply.github.com>
2022-11-28 17:04:50 +0500

    crptolist

---
+++ apis.ts
@@ -0,0 +1,31 @@
+import AnkrscanProvider from '@ankr.com/ankr.js';
+import type { Blockchain } from '@ankr.com/ankr.js/dist/types';
+
+const provider = new AnkrscanProvider('');
+
+//defining the list of supported blockchains
+const listOfChains: Blockchain[] = ['eth', 'arbitrum', 'avalanche', 
+'bsc', 'fantom', 'polygon', ];
+
+//key-value pair mapping of chains to their native symbols
+export const chainsToNativeSymbols: { [key in Blockchain]: string } = {
+  eth: 'ETH',
+  arbitrum: 'ETH',
+  avalanche: 'AVAX',
+  bsc: 'BNB',
+  fantom: 'FTM',
+  polygon: 'MATIC',
+  syscoin: 'SYS',
+  optimism: 'Opt',
+};
+
+//getCurrencies function
+export const getCurrencies = async (
+
+  blockchain: Blockchain
+) => {
+  return provider.getCurrencies({
+
+    blockchain,
+  });
+};
